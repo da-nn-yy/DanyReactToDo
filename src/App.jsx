dfec2b06f1b3b6effae418391a8bab5bdc39e9af
da-nn-yy <db7394928@gmail.com>
2024-06-29 08:56:47 +0200

    modified: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
 import './styles.css'
+import  { useState } from 'react';
 
 function App() {
 
+  const [newTask,setNewTask] = useState("");
 
+  const handleChange = (e) => {
+    setNewTask(e.target.value);
+    
+
+  }
   return (
     <>
-    <h1>React App</h1>
+    <h1>Enuty Page</h1>
+    <input type="text" onChange={handleChange}/>
+
+    <p>{newTask}</p>
     </>
   )
 }
