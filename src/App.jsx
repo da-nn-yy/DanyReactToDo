aaf4bf1ff884bab402efba6ed64b0d1c48a410e7
da-nn-yy <db7394928@gmail.com>
2024-06-29 09:35:30 +0200

    modified: App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,24 @@ import './styles.css'
 
 export default function App() {
   return (
-   "hi"
+   <>
+    <form className='new-item-form' action="">
+      <div className="form-row">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item"/>
+      </div>
+      <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      <li>
+        <label htmlFor="">
+          <input type="checkbox"/>
+          Item 1
+        </label>
+        <button className="btn btn-delete">Delete</button>
+      </li>
+    </ul>
+   </>
   )
 }
